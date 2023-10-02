7dc3874f789008e9b3de5a5184de051f67dba7ec
JGugino <gugino.joshua@gmail.com>
2023-10-01 22:19:39 -0400

    Started work on ui(def gonna change)

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,22 +1,20 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("node:path");
 
 const createWindow = () => {
 	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 1200,
+		height: 705,
+		resizable: false,
 		webPreferences: {
 			preload: path.join(__dirname, "preload.js"),
 		},
 	});
 
-	win.webContents.openDevTools();
-
 	win.loadFile("src/index.html");
 };
 
 app.whenReady().then(() => {
-	ipcMain.handle("ping", () => "pong");
 	createWindow();
 
 	app.on("window-all-closed", () => {
